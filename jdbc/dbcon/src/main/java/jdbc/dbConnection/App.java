f486c83f4332998ea45c3bd4dff0b88888c02fdc
Mohamed-ELgazzar <mhmd.atef.elgazzar@gmail.com>
2023-01-13 11:24:21 +0200

    first lab of jdbc

---
+++ jdbc/dbcon/src/main/java/jdbc/dbConnection/App.java
@@ -0,0 +1,31 @@
+package jdbc.dbConnection;
+
+
+import java.sql.*;
+import com.mysql.cj.jdbc.*;
+
+public class App {
+    public  App() {
+
+        String url = "jdbc:mysql://localhost:3306/connect";
+        String user = "root";
+        String password = "mody";
+        try {
+            DriverManager.registerDriver(new  com.mysql.cj.jdbc.Driver());
+            
+            Connection connection = DriverManager.getConnection(url, user, password);
+            System.out.println("Connection is Successful to the database" + url);
+
+          
+            connection.close();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void main(String args[]) {
+        new App();
+    }
+
+}
