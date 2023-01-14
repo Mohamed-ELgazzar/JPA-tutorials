82028d44e983607745c8a252dbe9159838fed132
Eslam Ebrahim <eslameprahim777@gmail.com>
2023-01-14 11:57:10 +0200

    eses

--- jdbc/dbcon/src/main/java/jdbc/dbConnection/App.java
+++ jdbc/dbcon/src/main/java/jdbc/dbConnection/App.java
@@ -26,7 +26,7 @@ public class App {
 
     public static void main(String args[]) {
         new App();
-        System.out.println("Mahmoud changed");
+        System.out.println("Mahmoud changed and Eslam");
     }
 
 }
